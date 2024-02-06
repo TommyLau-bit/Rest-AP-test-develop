21cd53391859cb4e544742151b4ac971a291ccfd
TommyLau-bit <tommyllk2003@gmail.com>
2024-02-06 17:14:10 +0000

    Initial Commit

--- src/__init__.py
+++ src/__init__.py
@@ -17,10 +17,10 @@ migrate = Migrate()
 def create_app(test_config=None):
     # create the Flask app
     app = Flask(__name__, instance_relative_config=True)
-    # configure the Flask app (see later notes on how to generate your own SECRET_KEY)
+    # configure the Flask app using the config.py file
     app.config.from_mapping(
         SECRET_KEY='jvLgWzRRFHcnNE8u2ClbMQ',
-        # Set the location of the database file called paralympics.sqlite which will be in the app's instance folder
+        # Set the location of the database file called data.sqlite
         SQLALCHEMY_DATABASE_URI= "sqlite:///" + os.path.join(app.instance_path, 'data.sqlite'),  
     )
 
@@ -36,11 +36,11 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-# Initialise Flask with the SQLAlchemy database extension
+    # Initialise Flask with the SQLAlchemy database extension
     db.init_app(app)
     migrate.init_app(app, db)
 
- # Initialise Flask-Marshmallow
+    # Initialise Flask-Marshmallow
     ma.init_app(app)
 
     # Create the tables in the database and register the routes
@@ -51,14 +51,14 @@ def create_app(test_config=None):
         # Add the data to the database if not already added
         add_data_from_csv()
 
- # Register the routes from paralympics.py with the app
+    # Register the routes from the routes module
 
         from src import routes # Import routes from the routes module
     return app
 
 
 
-def add_data_from_csv():
+def add_data_from_csv(): 
     import csv
     from pathlib import Path
 
@@ -81,7 +81,6 @@ def add_data_from_csv():
                     request_received_month=row[3],
                     request_closed_year=row[4],
                     request_closed_month=row[5],
-                    # ... other fields as per your Case model
                 )
                 db.session.add(case)
             db.session.commit()  # Commit after adding all cases
