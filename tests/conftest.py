80d383b5124e4d5eab5e0f8fa6c3685fca041ae1
TommyLau-bit <146777147+TommyLau-bit@users.noreply.github.com>
2024-11-05 15:42:00 +0000

    Add example.py to repository

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from pathlib import Path
 @pytest.fixture(scope='session') # the fixture is created once for the entire test session
 def app():
     """Create and configure a new app instance for the test session."""
-    db_path = Path(__file__).parent / 'data_testdb.sqlite'  # name of the test database is different from the production database
+    db_path = Path(__file__).parent / 'data_testdb.sqlite'  # name of the test database is different from the main database
     test_config = {
         'TESTING': True,
         'SQLALCHEMY_DATABASE_URI': f'sqlite:///{db_path}'
